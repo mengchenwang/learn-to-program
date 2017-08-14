c717f2ec40b1a54be0c5d1976025cb456140c9ee
mengchenwang <mengchenwang@hotmail.com>
2017-08-14 15:31:54 +0100

    chap02

---
+++ chap02/ex1.rb
@@ -0,0 +1,12 @@
+print "Hours in a year = "
+puts 24 * 365
+
+print "Minutes in a decade = "
+puts 60 * 24 * 365 * 10
+
+print "My age in seconds = "
+puts 60 * 60 * 24 * 365 * 34
+
+print "Author's age = "
+puts 1160000000 / 60 / 60 / 24 / 365
+ 
